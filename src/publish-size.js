10b276858c0c0a2caefea0136bf41b1e6b927dfe
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-07-31 14:10:12 +0530

    revert: fetch to axios

--- src/publish-size.js
+++ src/publish-size.js
@@ -1,4 +1,5 @@
 const { repo, sha, event, branch, pull_request_number, ci } = require('ci-env');
+const axios = require('axios');
 const SIZE_STORE_ENDPOINT = process.env.SIZE_STORE_ENDPOINT || 'https://size-plugin-store.now.sh' ;
 
 // TODO: add option to turn off publishing of sizes.
@@ -7,10 +8,7 @@ async function publishDiff(diff,filename) {
 	if (process.env.NODE_ENV !=='test' &&  ci && event == 'pull_request') {
 		try {
 			const params = { ci,repo, branch, sha, pull_request_number, diff,filename };
-			await fetch(`${SIZE_STORE_ENDPOINT}/diff`, {
-				method: 'POST',
-				body: JSON.stringify(params)
-			});
+			await axios.post(`${SIZE_STORE_ENDPOINT}/diff`, params);
 		}
 		catch (error) {
 			console.error('error: while publishing diff', error);
@@ -22,10 +20,7 @@ async function publishSizes(size,filename) {
 	if (process.env.NODE_ENV !=='test' &&  ci && event == 'push' && branch==='master') {
 		try {
 			const params = { ci,repo, branch, sha, pull_request_number, size,filename };
-			await fetch(`${SIZE_STORE_ENDPOINT}/size`, {
-				method: 'POST',
-				body: JSON.stringify(params)
-			});
+			await axios.post(`${SIZE_STORE_ENDPOINT}/size`, params);
 		}
 		catch (error) {
 			console.error('error: while publishing sizes', error);
